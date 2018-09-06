1bd77a167bda59d152f4688e9ccd1e7a67cca4a6
william.ence <william.ence@CLC-LAB209-16.TCSD.tooelesd.org>
2018-09-06 14:04:39 -0600

    Ch2exe

---
+++ src/WillE/ch2exe/Ch2Exe.java
@@ -0,0 +1,16 @@
+package WillE.ch2exe;
+/*
+ * Will Ence
+ * 9/6/18
+ * ch2 exe 
+ */
+
+public class Ch2Exe {
+
+	public static void main(String[] args) {
+		// display some text
+		System.out.println("Hello there...");
+		
+	}
+
+}
